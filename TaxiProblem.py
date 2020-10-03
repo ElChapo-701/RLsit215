e21fa99a687b83410adebc96aaf735b1497dd792
ElChapo-701 <62786695+ElChapo-701@users.noreply.github.com>
2020-10-03 18:05:38 +1000

    Add files via upload

---
+++ TaxiProblem.py
@@ -0,0 +1,131 @@
+import sys
+from contextlib import closing
+from io import StringIO
+from gym import utils
+from gym.envs.toy_text import discrete
+import numpy as np
+
+MAP = [
+    "+---------+",
+    "|R: | : :G|",
+    "| : | : : |",
+    "| : : : : |",
+    "| | : | : |",
+    "|Y| : |B: |",
+    "+---------+",
+]
+
+
+class TaxiEnv(discrete.DiscreteEnv):
+    
+    metadata = {'render.modes': ['human', 'ansi']}
+
+    def __init__(self):
+        self.desc = np.asarray(MAP, dtype='c')
+
+        self.locs = locs = [(0,0), (0,4), (4,0), (4,3)]
+
+        num_states = 500
+        num_rows = 5
+        num_columns = 5
+        max_row = num_rows - 1
+        max_col = num_columns - 1
+        initial_state_distrib = np.zeros(num_states)
+        num_actions = 6
+        P = {state: {action: []
+                     for action in range(num_actions)} for state in range(num_states)}
+        for row in range(num_rows):
+            for col in range(num_columns):
+                for pass_idx in range(len(locs) + 1):  # +1 for being inside taxi
+                    for dest_idx in range(len(locs)):
+                        state = self.encode(row, col, pass_idx, dest_idx)
+                        if pass_idx < 4 and pass_idx != dest_idx:
+                            initial_state_distrib[state] += 1
+                        for action in range(num_actions):
+                            # defaults
+                            new_row, new_col, new_pass_idx = row, col, pass_idx
+                            reward = -1 # default reward when there is no pickup/dropoff
+                            done = False
+                            taxi_loc = (row, col)
+
+                            if action == 0:
+                                new_row = min(row + 1, max_row)
+                            elif action == 1:
+                                new_row = max(row - 1, 0)
+                            if action == 2 and self.desc[1 + row, 2 * col + 2] == b":":
+                                new_col = min(col + 1, max_col)
+                            elif action == 3 and self.desc[1 + row, 2 * col] == b":":
+                                new_col = max(col - 1, 0)
+                            elif action == 4:  # pickup
+                                if (pass_idx < 4 and taxi_loc == locs[pass_idx]):
+                                    new_pass_idx = 4
+                                else: # passenger not at location
+                                    reward = -10
+                            elif action == 5:  # dropoff
+                                if (taxi_loc == locs[dest_idx]) and pass_idx == 4:
+                                    new_pass_idx = dest_idx
+                                    done = True
+                                    reward = 20
+                                elif (taxi_loc in locs) and pass_idx == 4:
+                                    new_pass_idx = locs.index(taxi_loc)
+                                else: # dropoff at wrong location
+                                    reward = -10
+                            new_state = self.encode(
+                                new_row, new_col, new_pass_idx, dest_idx)
+                            P[state][action].append(
+                                (1.0, new_state, reward, done))
+        initial_state_distrib /= initial_state_distrib.sum()
+        discrete.DiscreteEnv.__init__(
+            self, num_states, num_actions, P, initial_state_distrib)
+
+    def encode(self, taxi_row, taxi_col, pass_loc, dest_idx):
+     
+        i = taxi_row
+        i *= 5
+        i += taxi_col
+        i *= 5
+        i += pass_loc
+        i *= 4
+        i += dest_idx
+        return i
+
+    def decode(self, i):
+        out = []
+        out.append(i % 4)
+        i = i // 4
+        out.append(i % 5)
+        i = i // 5
+        out.append(i % 5)
+        i = i // 5
+        out.append(i)
+        assert 0 <= i < 5
+        return reversed(out)
+
+    def render(self, mode='human'):
+        outfile = StringIO() if mode == 'ansi' else sys.stdout
+
+        out = self.desc.copy().tolist()
+        out = [[c.decode('utf-8') for c in line] for line in out]
+        taxi_row, taxi_col, pass_idx, dest_idx = self.decode(self.s)
+
+        def ul(x): return "_" if x == " " else x
+        if pass_idx < 4:
+            out[1 + taxi_row][2 * taxi_col + 1] = utils.colorize(
+                out[1 + taxi_row][2 * taxi_col + 1], 'yellow', highlight=True)
+            pi, pj = self.locs[pass_idx]
+            out[1 + pi][2 * pj + 1] = utils.colorize(out[1 + pi][2 * pj + 1], 'blue', bold=True)
+        else:  
+            out[1 + taxi_row][2 * taxi_col + 1] = utils.colorize(
+                ul(out[1 + taxi_row][2 * taxi_col + 1]), 'green', highlight=True)
+
+        di, dj = self.locs[dest_idx]
+        out[1 + di][2 * dj + 1] = utils.colorize(out[1 + di][2 * dj + 1], 'magenta')
+        outfile.write("\n".join(["".join(row) for row in out]) + "\n")
+        if self.lastaction is not None:
+            outfile.write("  ({})\n".format(["South", "North", "East", "West", "Pickup", "Dropoff"][self.lastaction]))
+        else: outfile.write("\n")
+
+        
+        if mode != 'human':
+            with closing(outfile):
+                return outfile.getvalue()
